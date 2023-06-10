01c3e849675bb6f36c2d5cc2b97ae62b8ccca76a
Herby <herbyosbourne31@gmail.com>
2023-06-09 21:35:21 -0500

    create default export index

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import Hero from './Hero';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import CustomButton from './CustomButton';
+
+export { Hero, CustomButton, NavBar, Footer };
